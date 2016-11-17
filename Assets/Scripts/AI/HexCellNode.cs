53d86d604f495e1360387839801199aaf2c0ec26
MTesseracT <mtesseracttech@gmail.com>
2016-11-17 17:22:52 +0100

    Another Big Push, Adds the AI elements and IO stuff

---
+++ Assets/Scripts/AI/HexCellNode.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Saving
+{
+    [Serializable]
+    public class HexCellNode
+    {
+        public Color Color;
+        public Vector3 Coordinates;
+        public int Elevation;
+        public bool HasRiver;
+        public bool HasRoads;
+        public bool IsUnderWater;
+        public bool IsWalled;
+        public Vector3 Position;
+        public int[] NeighborIndexes;
+        public int Index;
+
+        public HexCellNode(HexCell cell, int index, List<HexCell> totalCellList)
+        {
+            Color = cell.Color;
+            Coordinates = new Vector3(cell.coordinates.X, cell.coordinates.Y, cell.coordinates.Z);
+            Elevation = cell.Elevation;
+            HasRiver = cell.HasRiver;
+            HasRoads = cell.HasRoads;
+            IsUnderWater = cell.IsUnderwater;
+            IsWalled = cell.Walled;
+            Position = cell.Position;
+            Index = index;
+
+            List<int> neighborIndexes = new List<int>();
+
+            HexDirection direction = HexDirection.NE;
+            do
+            {
+                HexCell neighbor = cell.GetNeighbor(direction);
+                if (neighbor != null)
+                {
+                    int neighborIndex = totalCellList.IndexOf(neighbor);
+                    neighborIndexes.Add(neighborIndex);
+                }
+                direction = direction.Next();
+            }
+            while (direction != HexDirection.NE);
+
+            NeighborIndexes = neighborIndexes.ToArray();
+
+        }
+
+        public override string ToString()
+        {
+            string neighbors = "";
+            foreach (var neighborIndex in NeighborIndexes) neighbors += neighborIndex + "\n";
+            return (
+                "HexCellNode " + Index + " info:\n" +
+                "Color - " + Color + "\n" +
+                "HexCoordinates - " + Coordinates + "\n"+
+                "Elevation - " + Elevation + "\n" +
+                "Contains River - " + HasRiver + "\n" +
+                "Contains Road -  " + HasRoads + "\n" +
+                "Is Underwater - " + IsUnderWater + "\n" +
+                "Is Walled - " + IsWalled + "\n" +
+                "Position - " + Position + "\n" +
+                "Index - " + Index + "\n" +
+                "Neighbor Indexes - \n" + neighbors
+            );
+        }
+    }
+}
---
+++ Assets/Scripts/IO/TerrainLoader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using System.IO;
+using Assets.Scripts.Saving;
+
+public class TerrainLoader : MonoBehaviour
+{
+
+    public GameObject HexNodesManagerRef;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (HexNodesManagerRef != null) LoadLevelData();
+            else
+            {
+                Debug.Log("The Hex Node Manager was not linked! Cannot Load Hex Node Data");
+            }
+        }
+    }
+
+    private void LoadLevelData()
+    {
+        HexCellNodeCollection loadedNodes =
+            JsonUtility.FromJson<HexCellNodeCollection>(FileAtPath("/ProceduralDump/Data/HexNodes.json"));
+        HexNodesManager hexNodeManager = HexNodesManagerRef.GetComponent<HexNodesManager>();
+        if (hexNodeManager != null)
+        {
+            hexNodeManager.SetNodes(loadedNodes.data, true);
+            Debug.Log("Successfully set the hexNode data!");
+        }
+        else
+        {
+            Debug.Log("The HexNodeManager reference does not have a HexNodeManager component!");
+        }
+    }
+
+    private string FileAtPath(string path)
+    {
+        try
+        {
+            string returnString = File.ReadAllText(Application.dataPath + path);
+            Debug.Log(Application.dataPath + path + "was successfully read!");
+            return returnString;
+        }
+        catch (Exception ex)
+        {
+            Debug.Log("An error occurred while reading: " + Application.dataPath + path + "\n" + ex);
+            return "";
+        }
+    }
+}
