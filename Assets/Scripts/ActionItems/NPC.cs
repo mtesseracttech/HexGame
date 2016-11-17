7bb6d0081132b9fb503006f249a0389036d48422
MTesseracT <mtesseracttech@gmail.com>
2016-11-18 00:46:06 +0100

    Fixed some shit

--- Assets/Scripts/ActionItems/NPC.cs
+++ Assets/Scripts/ActionItems/NPC.cs
@@ -5,13 +5,20 @@ using UnityEngine.UI;
 public class NPC : Interactable
 {
 
-    public string[] dialogue;
-    public string name;
-    public Sprite NPCImage;
+    //public string[] dialogue;
+    //public string name;
+   // public Sprite NPCImage;
+    public Dialogue dialogyue;
+
+    public void Start()
+    {
+        dialogyue = GetComponent<Dialogue>();
+    }
 
     public override void Interact()
     {
-        DialogueSystem.Instance.AddNewDialogue(dialogue,name,NPCImage);
+       // DialogueSystem.Instance.AddNewDialogue(dialogue,name,NPCImage);
+        dialogyue.ShowDialogue = true;
         Debug.Log("Interacting with NPC 2");
     }
 	
