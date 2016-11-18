d8ffda1b43a525ea7fedd94fcc0690f50a80a7da
KotrynaVysockyte <kaimelisvy24@outlook.com>
2016-11-18 11:29:59 +0200

    meh

--- Assets/Scripts/NPC/InstantiateDialog.cs
+++ Assets/Scripts/NPC/InstantiateDialog.cs
@@ -4,19 +4,19 @@ using System.Collections.Generic;
 
 public class InstantiateDialog : MonoBehaviour {
 
-    public TextAsset ta;
+    public TextAsset Ta;
     public Dialog dialog;
-    public int currentNode;
+    public int CurrentNode;
     public bool ShowDialogue;
 
-    public GUISkin skin;
+    public GUISkin Skin;
 
     public List<Answers> answers = new List<Answers>();
 
     void Start()
     {
-        dialog = Dialog.Load(ta);
-        skin = Resources.Load("Skin") as GUISkin;
+        dialog = Dialog.Load(Ta);
+        Skin = Resources.Load("Skin") as GUISkin;
     }
 
     void Update()
@@ -27,23 +27,23 @@ public class InstantiateDialog : MonoBehaviour {
     void UpdateAnswers()
     {
         answers.Clear();
-        for (int i = 0; i < dialog.nodes[currentNode].answers.Length; i++)
+        for (int i = 0; i < dialog.nodes[CurrentNode].answers.Length; i++)
         {
-            if (dialog.nodes[currentNode].answers[i].QuestName == null || dialog.nodes[currentNode].answers[i].NeedQuestValue == PlayerPrefs.GetInt(dialog.nodes[currentNode].answers[i].QuestName))
-                answers.Add(dialog.nodes[currentNode].answers[i]);
+            if (dialog.nodes[CurrentNode].answers[i].QuestName == null || dialog.nodes[CurrentNode].answers[i].NeedQuestValue == PlayerPrefs.GetInt(dialog.nodes[CurrentNode].answers[i].QuestName))
+                answers.Add(dialog.nodes[CurrentNode].answers[i]);
         }
     }
 
     void OnGUI()
     {
-        GUI.skin = skin;
+        GUI.skin = Skin;
         if (ShowDialogue)
         {
             GUI.Box(new Rect(Screen.width / 2 - 300, Screen.height - 300, 600, 250), "");
-            GUI.Label(new Rect(Screen.width / 2 - 250, Screen.height - 280, 500, 90), dialog.nodes[currentNode].NpcText);
+            GUI.Label(new Rect(Screen.width / 2 - 250, Screen.height - 280, 500, 90), dialog.nodes[CurrentNode].NpcText);
             for (int i = 0; i < answers.Count; i++)
             {
-                if (GUI.Button(new Rect(Screen.width / 2 - 250, Screen.height - 200 + 25 * i, 500, 25), answers[i].text, skin.label))
+                if (GUI.Button(new Rect(Screen.width / 2 - 250, Screen.height - 200 + 25 * i, 500, 25), answers[i].text, Skin.label))
                 {
                     if (answers[i].QuestValue > 0)
                     {
@@ -57,7 +57,7 @@ public class InstantiateDialog : MonoBehaviour {
                     {
                        // Character.Gold += answers[i].RewardGold;
                     }
-                    currentNode = answers[i].nextNode;
+                    CurrentNode = answers[i].nextNode;
                 }
             }
         }
