162071d2577d63bf2a38a0d5426c09f031a6ed1f
Bart-PC\Bart <bmisiulis@gmail.com>
2016-11-25 14:19:49 +0100

    Node Occupants

---
+++ Assets/Scripts/NodeGrid/MultiBuildingOccupant.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiBuildingOccupant : NodeOccupant {
+
+	public int[] nodeIndexes;
+
+	public override void Start () {		
+		_hexNodeManager = hexNodes.GetComponent <HexNodesManager> ();
+		SetOccupant ();
+	}
+
+	public override void SetOccupant () {
+		for (int i = 0; i < nodeIndexes.Length; i++) {
+			_hexNodeManager.GetHexNode (nodeIndexes[i]).Occupant = this;
+		}
+	}
+}
